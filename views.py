b5126c11aaf68617e4537638ecbbb95a9df6ae74
Alan YI <245655664@qq.com>
2018-03-04 01:38:50 +0800

    Changing index and views

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render,render_to_response
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render_to_response("index.html");
