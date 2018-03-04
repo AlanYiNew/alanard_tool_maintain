4d2ec1fd5cded89b80048b7305e088b9624d578f
Alan YI <245655664@qq.com>
2018-03-04 15:02:50 +0800

    Changing index and views

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from django.conf.urls.static import static
+from alanard_web_node import settings
 
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-]
+urlpatterns = [] + static("/", document_root=settings.ALANARDTOOLMAINTAIN_HOME_ROOT)
 
